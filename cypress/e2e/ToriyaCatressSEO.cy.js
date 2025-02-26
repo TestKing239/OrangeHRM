668776d2ca65dcd8a82135ad0da2f95c01b4d846
vishal-sahare95 <saharevishal41@gmail.com>
2025-02-27 00:42:30 +0530

    add new file

---
+++ cypress/e2e/ToriyaCatressSEO.cy.js
@@ -0,0 +1,29 @@
+const { title } = require("process");
+
+
+describe('NKDailyUpdate', () => {
+
+it('Login', () => {
+cy.visit('https://www.justdial.com/')
+cy.get('#main-auto').type('Toriya Cateress').type('{downarrow}').type('{enter}')
+
+cy.scrollTo('center'); // Scroll to the center of the page
+
+
+    })
+
+
+
+    it('Website', () => {
+  cy.visit('https://toriyacateringserviceakola.tiiny.site/')
+  cy.wait(5000)
+    })
+
+    it('Login', () => {
+        cy.visit('https://www.google.com/maps')
+        cy.get('#searchboxinput').type('Toriya Cateress Akola').type('{enter}')
+        cy.screenshot('home_page');
+
+          })
+
+})
