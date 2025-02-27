665ee8fa7365cbe40fa088a48d840aebef409d97
vishal-sahare95 <saharevishal41@gmail.com>
2025-02-28 01:54:56 +0530

    Update code

--- cypress/e2e/ToriyaCatressSEO.cy.js
+++ cypress/e2e/ToriyaCatressSEO.cy.js
@@ -6,7 +6,7 @@ describe('ToriyaCaterssSEO', () => {
 it('Login', () => {
 cy.visit('https://www.justdial.com/')
 cy.get('#main-auto').type('Toriya Cateress').type('{downarrow}').type('{enter}')
-
+cy.wait(50000)
 cy.scrollTo('center'); // Scroll to the center of the page
 
 
@@ -16,12 +16,21 @@ cy.scrollTo('center'); // Scroll to the center of the page
 
     it('Website', () => {
   cy.visit('https://toriyacateringserviceakola.tiiny.site/')
-  cy.wait(5000)
+  cy.wait(50000)
+  y.scrollTo('top')
+  cy.wait(50000)
+  y.scrollTo('center')
+  cy.wait(50000)
+  cy.scrollTo('bottom')
     })
 
-    it('Login', () => {
+    it('Map', () => {
         cy.visit('https://www.google.com/maps')
         cy.get('#searchboxinput').type('Toriya Cateress Akola').type('{enter}')
+        cy.wait(50000)
+        cy.get('.G7m0Af').click()
+        cy.scrollTo('top')
+        cy.wait(50000)
         cy.screenshot('home_page');
 
           })
